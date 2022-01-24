48194fd30c3da9f08f636e1130e2e3f04592c46b
bachir18 <75697872+bachir18@users.noreply.github.com>
2022-01-24 22:52:57 +0100

    My first commit

---
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,30 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+@Bean
+    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration repositoryRestConfiguration){
+        repositoryRestConfiguration.exposeIdsFor(Customer.class);
+        return args -> {
+            customerRepository.save(new Customer(null,"bachir","bachir@"));
+            customerRepository.save(new Customer(null,"mhr","mhr@"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());});
+
+        };
+    }
+}
